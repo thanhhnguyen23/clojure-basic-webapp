6675f209f7fb7ab359b11811f762ad224dc6fcbf
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-05 00:00:03 -0800

    add welcome handler

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,13 +1,16 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as webserver]))
 
+(defn welcome
+  "a ring handler to process all requests sent to the webapp"
+  [request]
+  {:status 200
+   :headers {}
+   :body "<html> <body> <h1>Hello, Clojure World!</h1> <p>Welcome to your first Clojure app!</p> <p>This message is returned regardless of the request</p> </body> </html>"})
+
 (defn -main
-  "simple web server using ring/jetty"
   [port-number]
   (webserver/run-jetty
-   (fn [request]
-     {:status 200
-      :headers {}
-      :body "<html><body><h1>Hello, Clojure World!</h1><h2>Welcome to your first Clojure App</h2<h3>This message is returned regardless of the request</h3></body></html>"})
+   welcome
    {:port (Integer. port-number)
     :join? false}))
