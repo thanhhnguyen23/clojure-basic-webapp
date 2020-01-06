5fa666f048b089c46198113d7d37ab523a758650
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-05 22:38:36 -0800

    add a defroutes function

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,23 +1,34 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as webserver]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+
+            ;; add compojure to namespace
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
+
 
 (defn welcome
-  "a ring handler to process all requests sent to the webapp. if a request is for something other than `/` then an error message is returned"
+  "a ring handler to respond witha simple welcome message"
   [request]
-  (if (= "/" (:uri request))
     {:status 200
      :headers {}
-     :body "<html> <body> <h1>Hi Thanh! Welcome to the Clojure World!</h1> <p>Welcome to your first Clojure app!</p> <p>This message is returned regardless of the request</p> </body> </html>"}
+     :body "<html> <body> <h1>Hi Thanh! Welcome to the Clojure World!</h1> <p>Welcome to your first Clojure app!</p>
+<p>Now this updates automatically.</p>
+</body> </html>"
+     })
 
-  {:status 404
-   :body "<html> <body> <h1>ERROR</h1> <p>This is probably not the page you are looking for.</p> <p>Requested page was not found</p> </body> </html>"
-   :headers {}}))
+(defroutes app
+  (GET "/" [] welcome)
+  (not-found " <h1>This is not the page you are looking for</h1><p>The page you are requested was not found.</p> "))
 
-(defn -dev-main
-  ;; -main
+(defn -main
+  "A very simple web server using Ring & Jetty"
   [port-number]
   (webserver/run-jetty
-   (wrap-reload #'welcome)
-   {:port (Integer. port-number)
-    :join? false}))
+   {:port (Integer. port-number)}))
+
+(defn -dev-main
+  "A very simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
+  [port-number]
+  (webserver/run-jetty (wrap-reload #'app)
+   {:port (Integer. port-number)}))
