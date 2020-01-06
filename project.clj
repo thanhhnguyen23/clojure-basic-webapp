4fde1ec471fa4f49225d2b9ff417611db497a8eb
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-05 22:37:11 -0800

    add compojure dep

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.8.0"]]
+                 [ring "1.8.0"]
+                 [compojure "1.6.1"]]
   :repl-options {:init-ns todo-list.core}
 
   ;; add profile to project configuration
